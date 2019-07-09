8235c1d38cf7860308c29f90a602ec424ea220e9
Luc Cary <luc.cary@gmail.com>
2019-07-08 21:57:20 -0400

    add important note to dreamcore executable

--- bin/executable.jl
+++ bin/executable.jl
@@ -7,6 +7,9 @@ To compile, clone the repo, cd into, add its deps via julia CLI and then run:
     julia --project ./juliac.jl -aeq ../DreamCore.jl/bin/executable.jl --outname dreamcore
     ./builddir/dreamcore
 
+Note: the dreamcore executable comes with a dreamcore.dylib file, so both files
+must be present in the same directory wherever they need to be executed.
+
 Tested with PackageCompiler@602f2b093308ac6cb6385d2dc5f4bf9f6a8e514e
 """
 module DreamCoreExecutable
