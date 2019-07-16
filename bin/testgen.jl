84d1143c3866565df11420e05e90511258278fd1
Luc Cary <luc.cary@gmail.com>
2019-07-16 16:50:16 -0400

    Improve testgen options and formatting

--- bin/testgen.jl
+++ bin/testgen.jl
@@ -6,6 +6,7 @@ module TestGenerator
 using JSON
 
 using DreamCore
+using DreamCore.Types: tint, tlist, arrow, tbool
 
 filename = "request_enumeration_example_2.json"
 filepath = abspath(@__DIR__, "..", "test", "resources", filename)
@@ -13,20 +14,29 @@ data = JSON.parsefile(filepath)
 primitives = base_primitives()
 grammar = Grammar(data["DSL"], primitives)
 problems = map(Problem, data["tasks"])
-type = problems[1].type
-# program_timeout = 5.0
-program_timeout = 0.5
+
+# request = problems[1].type
+request = tint
+# request = arrow(tlist(tint), tlist(tint))
+# request = tbool
+# request = tlist(tint)
+
+program_timeout = 5.0
+# program_timeout = 0.5
+
 # max_depth = 99
 max_depth = 5
 
 # warm the JIT cache
-r = generator(grammar, [], type, 3.0, 1.5, max_depth, false)
-take!(r)
+println("\nGenerate single program with upper=3.0, lower=1.5")
+r = generator(grammar, [], request, 3.0, 1.5, max_depth, false)
+r1 = take!(r)
+println("=> ", r1)
 
 start_time = time()
 
-println("\nGenerate programs with upper=3.0, lower=1.5")
-r = generator(grammar, [], type, 3.0, 1.5, max_depth, false)
+println("\nGenerate $program_timeout seconds of programs with upper=3.0, lower=1.5")
+r = generator(grammar, [], request, 3.0, 1.5, max_depth, false)
 for i in r
     println(string("=> ", i))
     if time() > start_time + program_timeout
@@ -35,8 +45,10 @@ for i in r
     end
 end
 
-println("\nGenerate programs with upper=6.0, lower=4.5")
-r = generator(grammar, [], type, 6.0, 4.5, max_depth, false)
+start_time = time()
+
+println("\nGenerate $program_timeout seconds of programs with upper=6.0, lower=4.5")
+r = generator(grammar, [], request, 6.0, 4.5, max_depth, false)
 for i in r
     println(string("=> ", i))
     if time() > start_time + program_timeout
