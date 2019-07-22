37574dd1bdc4c64790eda6225ac0f8382e0abae3
Luc Cary <luc.cary@gmail.com>
2019-07-22 15:45:22 -0400

    improve benchmarking script

--- perf/benchmark4.jl
+++ perf/benchmark4.jl
@@ -8,21 +8,27 @@ using DreamCore.Generation: build_candidates
 
 using DreamCore
 
-f = "../dreamcoder-testing/messages/messages/ocaml_request_enumeration_PID26993_20190719_T155300.json"
+filepath = "../dreamcoder-testing/messages/messages/ocaml_request_enumeration_PID26993_20190719_T155300.json"
 
-data = JSON.parsefile(f)
+data = JSON.parsefile(filepath)
 primitives = base_primitives()
 grammar = Grammar(data["DSL"], primitives)
 problems = map(Problem, data["tasks"])
 type = problems[1].type
 env = Array{TypeField}([])
+upper_bound = 10.5
+lower_bound = 9.0
+max_depth = 99
+
+function f(grammar, env, type, upper_bound, lower_bound, max_depth)
+    gen = generator(grammar, env, type, upper_bound, lower_bound, max_depth)
+    return [i for i in gen]
+end
 
 # precompile
-gen = generator(grammar, env, type, 10.5, 9.0, 99)
-[i for i in gen]
+f(grammar, env, type, upper_bound, lower_bound, max_depth)
 
-gen = generator(grammar, env, type, 10.5, 9.0, 99)
-t = @benchmark [i for i in gen]
+t = @benchmark f(grammar, env, type, upper_bound, lower_bound, max_depth)
 
 dump(t)
 
